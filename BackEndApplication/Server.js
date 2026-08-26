65605da27525af3b2411b5b120da132822a4d82c
Sathi777333 <sathishs3377@gmail.com>
2026-08-26 06:05:04 +0000

    solution

--- BackEndApplication/Server.js
+++ BackEndApplication/Server.js
@@ -6,6 +6,8 @@ const leaveRoutes = require('./Routes/leaveRoutes')
 
 const express = require('express')
 
+const cors = require('cors')
+
 const db = require('./Config/db')
 const authRoutes = require('./Routes/authRoutes')
 const employeeRoutes = require('./Routes/employeeRoutes')
@@ -18,6 +20,10 @@ app.use(express.json())
 app.use('/api/auth', authRoutes)
 app.use('/api/employees', employeeRoutes)
 app.use('/api/dashboard', dashboardRoutes)
+
+app.use(cors())
+
+app.use(express.json())
 
 app.use('/api/leaves', leaveRoutes)
 
